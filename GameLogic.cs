8d96c910f0ddbd1f080e5b217f9221edc41fb59a
larisa249 <larisarancov6@gmail.com>
2025-03-30 22:50:49 +0300

    imbunatatire explozii

--- GameLogic.cs
+++ GameLogic.cs
@@ -3,119 +3,150 @@ using Silk.NET.Maths;
 using TheAdventure.Models;
 using TheAdventure.Models.Data;
 
-namespace TheAdventure;
-
-public class GameLogic
+namespace TheAdventure
 {
-    private readonly Dictionary<int, GameObject> _gameObjects = new();
-    private readonly Dictionary<string, TileSet> _loadedTileSets = new();
-    private readonly Dictionary<int, Tile> _tileIdMap = new();
-    private Level _currentLevel = new();
-
-    private int _bombIds = 100;
-
-    public void InitializeGame()
+    public class GameLogic
     {
-        var levelContent = File.ReadAllText(Path.Combine("Assets", "terrain.tmj"));
-        var level = JsonSerializer.Deserialize<Level>(levelContent);
-        if (level == null)
-        {
-            throw new Exception("Failed to load level");
-        }
+        private readonly Dictionary<int, GameObject> _gameObjects = new();
+        private readonly Dictionary<string, TileSet> _loadedTileSets = new();
+        private readonly Dictionary<int, Tile> _tileIdMap = new();
+        private Level _currentLevel = new();
 
-        foreach (var tileSetRef in level.TileSets)
+        private int _bombIds = 100;
+
+        public void InitializeGame()
         {
-            var tileSetContent = File.ReadAllText(Path.Combine("Assets", tileSetRef.Source));
-            var tileSet = JsonSerializer.Deserialize<TileSet>(tileSetContent);
-            if (tileSet == null)
+            var levelContent = File.ReadAllText(Path.Combine("Assets", "terrain.tmj"));
+            var level = JsonSerializer.Deserialize<Level>(levelContent);
+            if (level == null)
             {
-                throw new Exception("Failed to load tile set");
+                throw new Exception("Failed to load level");
             }
 
-            foreach (var tile in tileSet.Tiles)
+            foreach (var tileSetRef in level.TileSets)
             {
-                tile.TextureId = GameRenderer.LoadTexture(Path.Combine("Assets", tile.Image), out _);
-                _tileIdMap.Add(tile.Id!.Value, tile);
-            }
-
-            _loadedTileSets.Add(tileSet.Name, tileSet);
-        }
-
-        _currentLevel = level;
-    }
-
-    public void ProcessFrame()
-    {
-    }
-
-    public void RenderAllObjects(int timeSinceLastFrame, GameRenderer renderer)
-    {
-        List<int> itemsToRemove = new List<int>();
-        foreach (var gameObject in GetRenderables())
-        {
-            if (gameObject.Update(timeSinceLastFrame))
-            {
-                gameObject.Render(renderer);
-            }
-            else
-            {
-                itemsToRemove.Add(gameObject.Id);
-            }
-        }
-
-        foreach (var item in itemsToRemove)
-        {
-            _gameObjects.Remove(item);
-        }
-    }
-
-    public void AddBomb(int x, int y)
-    {
-        AnimatedGameObject bomb = new AnimatedGameObject(Path.Combine("Assets", "BombExploding.png"), 2, _bombIds, 13, 13, 1, x, y);
-        _gameObjects.Add(bomb.Id, bomb);
-        ++_bombIds;
-    }
-
-    public void RenderTerrain(GameRenderer renderer)
-    {
-        foreach (var currentLayer in _currentLevel.Layers)
-        {
-            for (int i = 0; i < _currentLevel.Width; ++i)
-            {
-                for (int j = 0; j < _currentLevel.Height; ++j)
+                var tileSetContent = File.ReadAllText(Path.Combine("Assets", tileSetRef.Source));
+                var tileSet = JsonSerializer.Deserialize<TileSet>(tileSetContent);
+                if (tileSet == null)
                 {
-                    int? dataIndex = j * currentLayer.Width + i;
-                    if (dataIndex == null)
+                    throw new Exception("Failed to load tile set");
+                }
+
+                foreach (var tile in tileSet.Tiles)
+                {
+                    tile.TextureId = GameRenderer.LoadTexture(Path.Combine("Assets", tile.Image), out _);
+                    _tileIdMap.Add(tile.Id!.Value, tile);
+                }
+
+                _loadedTileSets.Add(tileSet.Name, tileSet);
+            }
+
+            _currentLevel = level;
+        }
+
+        public void ProcessFrame()
+        {
+        }
+
+        public void RenderAllObjects(int timeSinceLastFrame, GameRenderer renderer)
+        {
+            List<int> itemsToRemove = new List<int>();
+            foreach (var gameObject in GetRenderables())
+            {
+                if (gameObject.Update(timeSinceLastFrame))
+                {
+                    gameObject.Render(renderer);
+                }
+                else
+                {
+                    itemsToRemove.Add(gameObject.Id);
+                }
+            }
+            foreach (var item in itemsToRemove)
+            {
+                if (_gameObjects.TryGetValue(item, out var obj) && obj is AnimatedGameObject animObj)
+                {
+                    animObj.OnDestroy?.Invoke(this);
+                }
+                _gameObjects.Remove(item);
+            }
+        }
+
+        public void AddBomb(int x, int y)
+        {
+            AnimatedGameObject bomb = new AnimatedGameObject(
+                Path.Combine("Assets", "BombExploding.png"),
+                durationInSeconds: 2,
+                id: _bombIds,
+                numberOfFrames: 13,
+                numberOfColumns: 13,
+                numberOfRows: 1,
+                x: x,
+                y: y
+            );
+            bomb.OnDestroy = logic =>
+            {
+                float explosionRadius = 100f; 
+                logic.TriggerBombExplosionChain(bomb, explosionRadius);
+            };
+
+            _gameObjects.Add(bomb.Id, bomb);
+            ++_bombIds;
+        }
+
+        public void TriggerBombExplosionChain(AnimatedGameObject sourceBomb, float explosionRadius)
+        {
+            foreach (var obj in _gameObjects.Values)
+            {
+                if (obj is AnimatedGameObject otherBomb && otherBomb != sourceBomb)
+                {
+                    float dx = otherBomb.X - sourceBomb.X;
+                    float dy = otherBomb.Y - sourceBomb.Y;
+                    float dist = MathF.Sqrt(dx * dx + dy * dy);
+                    if (dist <= explosionRadius)
                     {
-                        continue;
+                        otherBomb.ForceEndNow = true;
                     }
-
-                    var currentTileId = currentLayer.Data[dataIndex.Value] - 1;
-                    if (currentTileId == null)
-                    {
-                        continue;
-                    }
-
-                    var currentTile = _tileIdMap[currentTileId.Value];
-
-                    var tileWidth = currentTile.ImageWidth ?? 0;
-                    var tileHeight = currentTile.ImageHeight ?? 0;
-
-                    var sourceRect = new Rectangle<int>(0, 0, tileWidth, tileHeight);
-                    var destRect = new Rectangle<int>(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
-                    renderer.RenderTexture(currentTile.TextureId, sourceRect, destRect);
                 }
             }
         }
-    }
 
-    public IEnumerable<RenderableGameObject> GetRenderables()
-    {
-        foreach (var gameObject in _gameObjects.Values)
+        public void RenderTerrain(GameRenderer renderer)
         {
-            if (gameObject is RenderableGameObject renderableGameObject)
+            foreach (var currentLayer in _currentLevel.Layers)
             {
-                yield return renderableGameObject;
+                for (int i = 0; i < _currentLevel.Width; ++i)
+                {
+                    for (int j = 0; j < _currentLevel.Height; ++j)
+                    {
+                        int? dataIndex = j * currentLayer.Width + i;
+                        if (dataIndex == null)
+                            continue;
+
+                        var currentTileId = currentLayer.Data[dataIndex.Value] - 1;
+                        if (currentTileId == null)
+                            continue;
+
+                        var currentTile = _tileIdMap[currentTileId.Value];
+                        var tileWidth = currentTile.ImageWidth ?? 0;
+                        var tileHeight = currentTile.ImageHeight ?? 0;
+
+                        var sourceRect = new Rectangle<int>(0, 0, tileWidth, tileHeight);
+                        var destRect = new Rectangle<int>(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
+                        renderer.RenderTexture(currentTile.TextureId, sourceRect, destRect);
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<RenderableGameObject> GetRenderables()
+        {
+            foreach (var gameObject in _gameObjects.Values)
+            {
+                if (gameObject is RenderableGameObject renderableGameObject)
+                {
+                    yield return renderableGameObject;
+                }
             }
         }
     }
--- Models/AnimatedGameObject.cs
+++ Models/AnimatedGameObject.cs
@@ -1,58 +1,118 @@
 using Silk.NET.Maths;
 
-namespace TheAdventure.Models;
-
-public class AnimatedGameObject : RenderableGameObject
+namespace TheAdventure.Models
 {
-    private int _durationInSeconds;
-    private int _numberOfColumns;
-    private int _numberOfRows;
-    private int _numberOfFrames;
-    private int _timeSinceAnimationStart = 0;
-
-    private int _currentRow = 0;
-    private int _currentColumn = 0;
-    private int _rowHeight = 0;
-    private int _columnWidth = 0;
-    private int _timePerFrame;
-
-    public AnimatedGameObject(string fileName, int durationInSeconds, int id, int numberOfFrames, int numberOfColumns,
-        int numberOfRows, int x, int y) :
-        base(fileName, id)
+    public class AnimatedGameObject : RenderableGameObject
     {
-        _durationInSeconds = durationInSeconds;
-        _numberOfFrames = numberOfFrames;
-        _numberOfColumns = numberOfColumns;
-        _numberOfRows = numberOfRows;
+        private int _durationInSeconds;
+        private int _numberOfColumns;
+        private int _numberOfRows;
+        private int _numberOfFrames;
+        private int _timeSinceAnimationStart = 0;
+        private int _currentRow = 0;
+        private int _currentColumn = 0;
+        private int _rowHeight = 0;
+        private int _columnWidth = 0;
+        private int _timePerFrame;
+        private float _scaleFactor = 1.0f;
+        
+        public int X { get; set; }
+        public int Y { get; set; }
+        
+        public bool ForceEndNow { get; set; } = false;
+    
+        public Action<GameLogic>? OnDestroy { get; set; }
 
-        _rowHeight = TextureInformation.Height / numberOfRows;
-        _columnWidth = TextureInformation.Width / numberOfColumns;
+        public AnimatedGameObject(
+            string fileName,
+            int durationInSeconds,
+            int id,
+            int numberOfFrames,
+            int numberOfColumns,
+            int numberOfRows,
+            int x,
+            int y
+        )
+            : base(fileName, id)
+        {
+            _durationInSeconds = durationInSeconds;
+            _numberOfFrames = numberOfFrames;
+            _numberOfColumns = numberOfColumns;
+            _numberOfRows = numberOfRows;
 
-        var halfRow = _rowHeight / 2;
-        var halfColumn = _columnWidth / 2;
+            X = x;
+            Y = y;
 
-        _timePerFrame = (durationInSeconds * 1000) / _numberOfFrames;
+            _rowHeight = TextureInformation.Height / numberOfRows;
+            _columnWidth = TextureInformation.Width / numberOfColumns;
+            var halfRow = _rowHeight / 2;
+            var halfColumn = _columnWidth / 2;
+            _timePerFrame = (durationInSeconds * 1000) / _numberOfFrames;
+            TextureDestination = new Rectangle<int>(
+                X - halfColumn,
+                Y - halfRow,
+                _columnWidth,
+                _rowHeight
+            );
+            TextureSource = new Rectangle<int>(
+                _currentColumn * _columnWidth,
+                _currentRow * _rowHeight,
+                _columnWidth,
+                _rowHeight
+            );
+        }
 
-        TextureDestination =
-            new Rectangle<int>(x - halfColumn, y - halfRow, _columnWidth, _rowHeight);
-        TextureSource = new Rectangle<int>(_currentColumn * _columnWidth, _currentRow * _rowHeight, _columnWidth,
-            _rowHeight);
-    }
+        public override bool Update(int timeSinceLastFrame)
+        {
+            if (ForceEndNow)
+            {
+                _timeSinceAnimationStart += timeSinceLastFrame;
+                _scaleFactor += (float)timeSinceLastFrame / 1000f * 1.0f;
+                int newWidth = (int)(_columnWidth * _scaleFactor);
+                int newHeight = (int)(_rowHeight * _scaleFactor);
+                TextureDestination = new Rectangle<int>(
+                    X - newWidth / 2,
+                    Y - newHeight / 2,
+                    newWidth,
+                    newHeight
+                );
+                _currentRow = (_numberOfFrames - 1) / _numberOfColumns;
+                _currentColumn = (_numberOfFrames - 1) % _numberOfColumns;
+                TextureSource = new Rectangle<int>(
+                    _currentColumn * _columnWidth,
+                    _currentRow * _rowHeight,
+                    _columnWidth,
+                    _rowHeight
+                );
+                if (_timeSinceAnimationStart >= _durationInSeconds * 1000 + 1000)
+                    return false;
+                return true;
+            }
+            else
+            {
+                _timeSinceAnimationStart += timeSinceLastFrame;
+                if (_timeSinceAnimationStart >= _durationInSeconds * 1000)
+                    return false;
 
-    public override bool Update(int timeSinceLastFrame)
-    {
-        _timeSinceAnimationStart += timeSinceLastFrame;
-
-        var currentFrame = _timeSinceAnimationStart / _timePerFrame;
-
-        if (_timeSinceAnimationStart > _durationInSeconds * 1000) return false;
-
-        _currentRow = currentFrame / _numberOfColumns;
-        _currentColumn = currentFrame % _numberOfColumns;
-
-        TextureSource = new Rectangle<int>(_currentColumn * _columnWidth, _currentRow * _rowHeight, _columnWidth,
-            _rowHeight);
-
-        return true;
+                var currentFrame = _timeSinceAnimationStart / _timePerFrame;
+                _currentRow = currentFrame / _numberOfColumns;
+                _currentColumn = currentFrame % _numberOfColumns;
+                TextureSource = new Rectangle<int>(
+                    _currentColumn * _columnWidth,
+                    _currentRow * _rowHeight,
+                    _columnWidth,
+                    _rowHeight
+                );
+                var halfRow = _rowHeight / 2;
+                var halfColumn = _columnWidth / 2;
+                TextureDestination = new Rectangle<int>(
+                    X - halfColumn,
+                    Y - halfRow,
+                    _columnWidth,
+                    _rowHeight
+                );
+                return true;
+            }
+        }
     }
 }
