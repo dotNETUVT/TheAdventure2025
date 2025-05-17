79f4ef36e792563a638b719f239bd317667b648f
KarinaValentina19 <119106508+KarinaValentina19@users.noreply.github.com>
2025-05-17 18:37:26 +0300

    coin collect

--- GameObject.cs
+++ GameObject.cs
@@ -2,4 +2,16 @@ namespace TheAdventure;
 
 public class GameObject
 {
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Width { get; set; } = 32;
+    public int Height { get; set; } = 32;
+
+    public bool IsCollidingWith(GameObject other)
+    {
+        return X < other.X + other.Width &&
+               X + Width > other.X &&
+               Y < other.Y + other.Height &&
+               Y + Height > other.Y;
+    }
 }
